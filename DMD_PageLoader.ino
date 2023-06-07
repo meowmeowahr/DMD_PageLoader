e25f84de4d5c0777a5248e7dfb2c1a7f1bda7ba4
Kevin Ahr <meowmeowahr@gmail.com>
2023-06-07 14:30:55 -0500

    Cleanup

--- DMD_PageLoader.ino
+++ DMD_PageLoader.ino
@@ -38,10 +38,10 @@
 // SDCARD_SS_PIN is defined for the built-in SD on some boards.
 #ifndef SDCARD_SS_PIN
 const uint8_t SD_CS_PIN = SS;
-#else   // SDCARD_SS_PIN
+#else  // SDCARD_SS_PIN
 // Assume built-in SD is used.
 const uint8_t SD_CS_PIN = SDCARD_SS_PIN;
-#endif  // SDCARD_SS_PIN
+#endif // SDCARD_SS_PIN
 
 // Try max SPI clock for an SD. Reduce SPI_CLOCK if errors occur.
 #define SPI_CLOCK SD_SCK_MHZ(50)
@@ -51,9 +51,9 @@ const uint8_t SD_CS_PIN = SDCARD_SS_PIN;
 #define SD_CONFIG SdioConfig(FIFO_SDIO)
 #elif ENABLE_DEDICATED_SPI
 #define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SPI_CLOCK)
-#else  // HAS_SDIO_CLASS
+#else // HAS_SDIO_CLASS
 #define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SPI_CLOCK)
-#endif  // HAS_SDIO_CLASS
+#endif // HAS_SDIO_CLASS
 
 #if SD_FAT_TYPE == 0
 SdFat sd;
@@ -71,13 +71,13 @@ ExFile file;
 SdFs sd;
 FsFile dir;
 FsFile file;
-#else  // SD_FAT_TYPE
+#else // SD_FAT_TYPE
 #error invalid SD_FAT_TYPE
-#endif  // SD_FAT_TYPE
+#endif // SD_FAT_TYPE
 
 #define VERSION "0.1.0"
 
-SoftDMD dmd(1, 2);  // DMD controls the entire display(s)
+SoftDMD dmd(1, 2); // DMD controls the entire display(s)
 Buzzer buzzer(BUZZER_PIN, LED_BUILTIN);
 OneButton btn(ENC_BTN);
 Encoder enc(ENC_A, ENC_B);
