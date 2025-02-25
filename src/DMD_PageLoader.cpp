c0f83f6c55ae2dd010aef40e8ccea08cea7ee749
meowmeowahr <meowmeowahr@gmail.com>
2025-02-24 18:26:03 -0600

    exfat support, many things broken

--- src/DMD_PageLoader.cpp
+++ src/DMD_PageLoader.cpp
@@ -11,11 +11,14 @@ Author: Kevin Ahr
 #include <Buzzer.h>
 #include <EEPROMex.h>
 #include <MemoryFree.h>
+#include <ArduinoJson.h>
 
 #include "Arial_Black_16.h"
 #include "Droid_Sans_12.h"
 #include "NumberFont3x5.h"
 #include "SystemFont5x7.h"
+
+#include "Bitmaps.h"
 
 // SD_FAT_TYPE = 0 for SdFat/File as defined in SdFatConfig.h,
 // 1 for FAT16/FAT32, 2 for exFAT, 3 for FAT16/FAT32 and exFAT.
@@ -38,14 +41,16 @@ Author: Kevin Ahr
 // Try max SPI clock for an SD. Reduce SPI_CLOCK if errors occur.
 #define SPI_CLOCK SD_SCK_MHZ(200)
 
-// Try to select the best SD card configuration.
-#if HAS_SDIO_CLASS
+#if defined(HAS_TEENSY_SDIO)
 #define SD_CONFIG SdioConfig(FIFO_SDIO)
+#elif defined(RP_CLK_GPIO) && defined(RP_CMD_GPIO) && defined(RP_DAT0_GPIO)
+// See the Rp2040SdioSetup example for RP2040/RP2350 boards.
+#define SD_CONFIG SdioConfig(RP_CLK_GPIO, RP_CMD_GPIO, RP_DAT0_GPIO)
 #elif ENABLE_DEDICATED_SPI
 #define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SPI_CLOCK)
-#else // HAS_SDIO_CLASS
+#else  // HAS_TEENSY_SDIO
 #define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SPI_CLOCK)
-#endif // HAS_SDIO_CLASS
+#endif  // HAS_TEENSY_SDIO
 
 // SDCARD_SS_PIN is defined for the built-in SD on some boards.
 #ifndef SDCARD_SS_PIN
@@ -60,9 +65,9 @@ SdFat sd;
 File root;
 File file;
 #elif SD_FAT_TYPE == 1
-SdFat sd;
-FatFile root;
-FatFile file;
+SdFat32 sd;
+File32 root;
+File32 file;
 #elif SD_FAT_TYPE == 2
 SdExFat sd;
 ExFile root;
@@ -75,7 +80,7 @@ FsFile file;
 #error invalid SD_FAT_TYPE
 #endif // SD_FAT_TYPE
 
-#define VERSION "V100"
+#define VERSION "v2.0.0"
 
 SoftDMD dmd(1, 2); // DMD controls the entire display(s)
 ArduinoQueue<char *> fxQueue(FX_RX_Q);
@@ -87,12 +92,13 @@ unsigned int files = 0;
 
 unsigned long previousMillis = 0;
 int timebarPos = 1;
-int pageTime = 1000;
+int pageTime = 0;
 int pageTimeMult = 1;
 
 int brightness = 127;
 
 char *fx = (char *)malloc(FX_MAX_CMD);
+
 
 void wipeAni();
 
@@ -112,9 +118,35 @@ int euclidean_modulo(int a, int b);
 
 bool inRange(int val, int minimum, int maximum);
 
+void displayBitmap();
+
+uint16_t reverseBits(uint16_t num);
+
+uint16_t reverseBits(uint16_t num) {
+  uint16_t rev = 0;
+  for (int i = 0; i < 16; i++) {
+      rev <<= 1;
+      rev |= (num & 1);
+      num >>= 1;
+  }
+  return rev;
+}
+
+void displayBitmap(const uint16_t image_frame[]) {
+  for (int y = 0; y < 32; y++) {
+    for (int x = 0; x < 32; x++) {
+        int index = (y * 32 + x) / 16;  // Index in the uint16_t array
+        int bit_pos = x % 16;           // Bit position in the 16-bit word
+
+        uint16_t value = pgm_read_word(&image_frame[index]);  // Read word from PROGMEM
+        bool pixel = (value >> (15 - bit_pos)) & 1;  // Extract correct bit
+        dmd.setPixel(x, y, pixel ? GRAPHICS_ON : GRAPHICS_OFF);
+    }
+  }
+}
+
 void setup() {
   Serial.begin(FX_BAUD);
-  sd.initErrorPrint(&Serial);
 
   dmd.begin();
   dmd.setBrightness(brightness);
@@ -124,47 +156,39 @@ void setup() {
 
   loadSettings();
 
-  dispLoad(F("33"));
-
   // Initialize the SD.
   if (!sd.begin(SD_CONFIG)) {
-    // dispError(1);
-    // while (true) {
-    //   buzzer.sound(NOTE_C2, 100);
-    //   buzzer.sound(0, 50);
-    //   buzzer.sound(NOTE_C2, 100);
-    //   buzzer.sound(0, 150);
-    // }
-    // not sure why it returns false, yet success
+    dispError(1);
+    while (true) {
+    }
   }
-
-  dispLoad(F("67"));
 
   // Open root directory
   if (!root.open("/")) {
-    // dispError(2);
-    Serial.println("Fail2");
-    // while (true) {
-    //   buzzer.sound(NOTE_C2, 100);
-    //   buzzer.sound(0, 50);
-    //   buzzer.sound(NOTE_C2, 100);
-    //   buzzer.sound(0, 150);
-    // }
+    dispError(2);
+    while (true) {
+    }
   }
-  // if (root.getError()) {
-  //   dispError(3);
-  //   Serial.println("Fail3");
-  //   while (true) {
-  //     buzzer.sound(NOTE_C2, 100);
-  //     buzzer.sound(0, 50);
-  //     buzzer.sound(NOTE_C2, 100);
-  //     buzzer.sound(0, 150);
-  //   }
-  // }
 
-  dispLoad(F("100"));
+  // count the number of files ending in .DMD
+  char* filesChar = (char *)malloc(8);
+  while (true) {
+    int rc = file.openNext(&root, O_READ);
+    file.getName(fileName, sizeof(fileName));
+    if (EndsWith(fileName, ".DMD")) {
+      files++;
+      itoa(files, filesChar, 10);
+      dispLoad(filesChar);
+    }
+    file.close();
+    Serial.println(rc);
+    if (!rc) {
+      break;
+    }
+  }
 
-  // wipeAni();
+  displayBitmap(splash);
+  delay(4000);
 }
 
 void loop() {
@@ -193,17 +217,28 @@ void loop() {
     // parse command=value
     char *fxCmd = strtok(fxRaw, "=");
     char *fxVal = strtok(NULL, FX_LINE_ENDING_STR);
-    Serial.print("fxCmd=");
+    Serial.print("fxcmd=");
     Serial.println(fxCmd);
-    Serial.print("fxVal=");
+    Serial.print("fxval=");
     Serial.println(fxVal);
+
+    // switch case for commands
+    if (strcmp(fxCmd, "rewind") == 0) {
+      root.rewind();
+    } else if (strcmp(fxCmd, "V") == 0) {
+      Serial.print("version=");
+      Serial.println(VERSION);
+    } else if (strcmp(fxCmd, "formatsd") == 0) {
+      sd.format(&Serial);
+    }
+
   }
 
   // Attempt to open the next file
   int rc = file.openNext(&root, FILE_READ);
   if (!rc) {
     Serial.println("state=rewind");
-    root.rewindDirectory(); // Reset directory reading position
+    root.rewind(); // Reset directory reading position
     return;
   }
 
@@ -217,7 +252,7 @@ void loop() {
 
   if (EndsWith(fileName, ".DMD")) {
     if (true) {
-      file.readBytes(fileBuffer, 1025);
+      file.read(fileBuffer, 1025);
       loadPic(fileBuffer);
       if (pageTime > 0) {
         pageTimeMult = fileBuffer[0];
@@ -236,20 +271,50 @@ void wipeAni() {
 }
 
 void loadSettings() {
-  EEPROM.setMemPool(EEPROM_BASE, EEPROMSizeMega);
-  EEPROM.setMaxAllowedWrites(EEPROM_MAX_WRITES);
-  if (inRange(EEPROM.readInt(EEPROM_BASE), 0, 9990)) {
-    pageTime = EEPROM.readInt(EEPROM_BASE);
-  }
+  // EEPROM.setMemPool(EEPROM_BASE, EEPROMSizeMega);
+  // EEPROM.setMaxAllowedWrites(EEPROM_MAX_WRITES);
+  // if (inRange(EEPROM.readInt(EEPROM_BASE), 0, 9990)) {
+  //   pageTime = EEPROM.readInt(EEPROM_BASE);
+  // }
 
-  if (inRange(EEPROM.readByte(EEPROM_BASE + 2), 0, 2)) {
-    timebarPos = EEPROM.readByte(EEPROM_BASE + 2);
-  }
+  // if (inRange(EEPROM.readByte(EEPROM_BASE + 2), 0, 2)) {
+  //   timebarPos = EEPROM.readByte(EEPROM_BASE + 2);
+  // }
 
-  if (inRange(EEPROM.readByte(EEPROM_BASE + 3), 1, 255)) {
-    brightness = EEPROM.readByte(EEPROM_BASE + 3);
-    dmd.setBrightness(brightness);
-  }
+  // if (inRange(EEPROM.readByte(EEPROM_BASE + 3), 1, 255)) {
+  //   brightness = EEPROM.readByte(EEPROM_BASE + 3);
+  //   dmd.setBrightness(brightness);
+  // }
+  // instead of using EEPROM, use SD card for settings with json
+  // if (file.open("settings.json", FILE_READ)) {
+  //   StaticJsonDocument<256> doc;
+  //   DeserializationError error = deserializeJson(doc, file);
+  //   if (error) {
+  //     Serial.print(F("deserializeJson() failed: "));
+  //     Serial.println(error.c_str());
+  //     return;
+  //   }
+
+  //   pageTime = doc["pageTime"];
+  //   timebarPos = doc["timebarPos"];
+  //   brightness = doc["brightness"];
+  //   dmd.setBrightness(brightness);
+
+  //   file.close();
+  //   Serial.println("here");
+  // } else {
+  //   // create settings file
+  //   if (file.open("settings.json", FILE_WRITE)) {
+  //     StaticJsonDocument<256> doc;
+  //     doc["pageTime"] = pageTime;
+  //     doc["timebarPos"] = timebarPos;
+  //     doc["brightness"] = brightness;
+
+  //     serializeJson(doc, file);
+  //     file.close();
+  //   }
+  //   Serial.println("here2");
+  // }
 }
 
 void loadPic(const uint8_t *pic) {
@@ -297,8 +362,7 @@ void dispError(uint8_t code) {
 
 void dispLoad(const String &pcnt) {
   dmd.selectFont(Droid_Sans_12);
-  dmd.clearScreen();
-  dmd.drawString(0, 1, "LOAD");
+  dmd.drawString(0, 1, "FILE");
   dmd.drawString(0, 17, pcnt);
 }
 
